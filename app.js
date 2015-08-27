1716974eb3a477383ff1ce5bf50079922af34ab3
Sidney Wagner <herrwagner1680@gmail.com>
2015-08-27 12:32:46 -0400

    First commit

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var App = angular.module('App', []);
 
 App.controller('TodoCtrl', function($scope, $http) {
+	$scope.todos = [];
   $http.get('todos.json')
-       .then(function(res){
-          $scope.todos = res.data;                
-        });
+  		.success(function (data){
+  			$scope.todos = data;
+  		})
+  		.error(function (data, status, headers, config) {
+  			console.log("ERROR");
+  		});
+  		// })
+    //    .then(function(res){
+    //       $scope.todos = res.data;                
+    //     });
 });
